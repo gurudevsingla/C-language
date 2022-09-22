7929f400ea09daad52e236a13f1dea716b45b55b
Gurudev <109899687+gurudevsingla@users.noreply.github.com>
2022-09-22 18:04:26 +0530

    Add files via upload

---
+++ try.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    char ch;
+    z:  ch=getch();
+            switch(ch)
+            {
+                case 80 :
+                            printf("%d\n",ch);
+                            break;
+                case 77 :
+                            printf("%d\n",ch);
+                            break;
+                case 72 :
+                            printf("%d\n",ch);
+                            break;
+                case 75 :
+                            printf("%d\n",ch);
+                            break;
+                case 'e' : exit(0);
+                case 'E' : exit(0);
+               default : printf("Press an arrow key\n");
+            }
+            fflush(stdin);
+            goto z;
+            return 0;
+}
